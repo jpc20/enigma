01da2eccd7377347d20aa803cfd45d4569a6695d
Jack Cullen <jpcullen20@gmail.com>
2020-04-16 16:03:47 -0600

    Add tests from interaction pattern

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,49 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_can_encrypt_when_given_a_date
+    skip
+  expected = {
+    encryption: "keder ohulw",
+    key: "02715",
+    date: "040895"
+  }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_when_given_a_date
+    skip
+    expected = {
+          decryption: "hello world",
+          key: "02715",
+          date: "040895"
+    }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_with_todays_date
+    skip
+  expected = {
+    encryption: "keder ohulw",
+    key: "02715",
+    date: "040895"
+  }
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_decrypt_with_todays_date
+    skip
+    expected = {
+          decryption: "hello world",
+          key: "02715",
+          date: "040895"
+    }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715")
+  end
+
+  def test_it_can_encrypt_with_a_random_key_and_todays_date
+    skip
+    assert_equal expected, @enigma.encrypt("hello world")
+  end
 end

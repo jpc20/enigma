2dfe6b2dec4b50c301ccba83128973e345fdb2b7
Jack Cullen <jpcullen20@gmail.com>
2020-04-20 11:52:53 -0600

    Instantiate key_finder in decryptor and use key_finder in the find_key method

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -1,6 +1,12 @@
 require_relative "machine"
+require_relative "key_finder"
 
 class Decryptor < Machine
+
+  def initialize
+    @key_finder = Keyfinder.new
+    super
+  end
 
   def decrypt(encrypted_message, key, date = todays_date)
     {
@@ -78,69 +84,69 @@ class Decryptor < Machine
   end
 
   def find_key(encrypted_message, date)
-    combine_keys(find_working_keys(encrypted_message, date))
+    @key_finder.find_key(encrypted_message, date)
   end
 
-  def combine_keys(keys)
-    keys.each_with_index.map do |key, index|
-      if index == 0
-        key
-      else
-        key[1]
-      end
-    end.join
-  end
-
-  def find_working_keys(encrypted_message, date)
-    potential_keys = find_all_potential_keys(encrypted_message, date)
-    potential_keys[0].each do |key_1|
-      next if !key_is_valid?(potential_keys[1], key_1)
-      key_2 = find_next_valid_key(potential_keys[1], key_1)
-      next if !key_is_valid?(potential_keys[2], key_2)
-      key_3 = find_next_valid_key(potential_keys[2], key_2)
-      next if !key_is_valid?(potential_keys[3], key_3)
-      key_4 = find_next_valid_key(potential_keys[3], key_3)
-      return [key_1, key_2, key_3, key_4]
-    end
-  end
-
-  def key_is_valid?(potential_key_set, key)
-    potential_key_set.any?{|potential_key| potential_key[0] == key[1]}
-  end
-
-  def find_next_valid_key(potential_key_set, key)
-    potential_key_set.find{|potential_key| potential_key[0] == key[1]}
-  end
-
-  def find_all_potential_keys(encrypted_message, date)
-    all_potential_keys = Hash.new
-    shifts_minus_offsets(encrypted_message, date).each_with_index do |key_value, index|
-      all_potential_keys[index] = calculate_possible_key_values(key_value)
-    end
-    all_potential_keys
-  end
-
-  def calculate_possible_key_values(key_value)
-    [
-      first_key_value(key_value),
-      (key_value + 27).to_s,
-      (key_value + 54).to_s,
-      (key_value + 81).to_s,
-    ].reject{ |key_value| key_value.to_i >= 100}
-  end
-
-  def first_key_value(key)
-    return "0" + key.to_s if key.to_s.length == 1
-    key.to_s
-  end
-
-  def shifts_minus_offsets(encrypted_message, date)
-    offsets = date_to_offsets(date)
-    find_shifts(encrypted_message, date).each_with_index.map do |shift, index|
-      shifted = shift - offsets[index]
-      shifted %= 27 if shifted < 0
-      shifted
-    end
-  end
+  # def combine_keys(keys)
+  #   keys.each_with_index.map do |key, index|
+  #     if index == 0
+  #       key
+  #     else
+  #       key[1]
+  #     end
+  #   end.join
+  # end
+  #
+  # def find_working_keys(encrypted_message, date)
+  #   potential_keys = find_all_potential_keys(encrypted_message, date)
+  #   potential_keys[0].each do |key_1|
+  #     next if !key_is_valid?(potential_keys[1], key_1)
+  #     key_2 = find_next_valid_key(potential_keys[1], key_1)
+  #     next if !key_is_valid?(potential_keys[2], key_2)
+  #     key_3 = find_next_valid_key(potential_keys[2], key_2)
+  #     next if !key_is_valid?(potential_keys[3], key_3)
+  #     key_4 = find_next_valid_key(potential_keys[3], key_3)
+  #     return [key_1, key_2, key_3, key_4]
+  #   end
+  # end
+  #
+  # def key_is_valid?(potential_key_set, key)
+  #   potential_key_set.any?{|potential_key| potential_key[0] == key[1]}
+  # end
+  #
+  # def find_next_valid_key(potential_key_set, key)
+  #   potential_key_set.find{|potential_key| potential_key[0] == key[1]}
+  # end
+  #
+  # def find_all_potential_keys(encrypted_message, date)
+  #   all_potential_keys = Hash.new
+  #   shifts_minus_offsets(encrypted_message, date).each_with_index do |key_value, index|
+  #     all_potential_keys[index] = calculate_possible_key_values(key_value)
+  #   end
+  #   all_potential_keys
+  # end
+  #
+  # def calculate_possible_key_values(key_value)
+  #   [
+  #     first_key_value(key_value),
+  #     (key_value + 27).to_s,
+  #     (key_value + 54).to_s,
+  #     (key_value + 81).to_s,
+  #   ].reject{ |key_value| key_value.to_i >= 100}
+  # end
+  #
+  # def first_key_value(key)
+  #   return "0" + key.to_s if key.to_s.length == 1
+  #   key.to_s
+  # end
+  #
+  # def shifts_minus_offsets(encrypted_message, date)
+  #   offsets = date_to_offsets(date)
+  #   find_shifts(encrypted_message, date).each_with_index.map do |shift, index|
+  #     shifted = shift - offsets[index]
+  #     shifted %= 27 if shifted < 0
+  #     shifted
+  #   end
+  # end
 
 end
--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -85,48 +85,48 @@ class DecryptorTest < Minitest::Test
     assert_equal 14, @decryptor.shift_amount("s", "e")
   end
 
-  def test_find_key_with_date_and_encrypted_message
-    assert_equal "08304", @decryptor.find_key("vjqtbeaweqihssi", "291018")
-    assert_equal "14140", @decryptor.find_key("mzwet fmuen'owek'rwzifefuegmfwfeuys.
-     gmgwsqlrgattsmyeqdrggrx!!!twaq", "190420")
-    assert_equal "14811", @decryptor.find_key("mfictgskul 'ocri'yiximrdultkfcscuee.
-    gtkgceolytzt ekylcbrntpx!!!tcno", "200420")
-  end
-
-
-  def test_find_all_potential_keys
-    expected = {0=>["08", "35", "62", "89"], 1=>["02", "29", "56", "83"], 2=>["03", "30", "57", "84"], 3=>["04", "31", "58", "85"]}
-    assert_equal expected, @decryptor.find_all_potential_keys("vjqtbeaweqihssi", "291018")
-  end
-
-  def test_find_working_keys
-    assert_equal ["08", "83", "30", "04"], @decryptor.find_working_keys("vjqtbeaweqihssi", "291018")
-  end
-
-  def test_combine_keys
-    assert_equal "08304", @decryptor.combine_keys(["08", "83", "30", "04"])
-  end
-
-  def test_key_is_valid
-    assert_equal true, @decryptor.key_is_valid?(["02", "29", "56", "83"], "08")
-    assert_equal false, @decryptor.key_is_valid?(["02", "29", "56", "83"], "89")
-  end
-
-  def test_find_next_valid_key
-    assert_equal "83", @decryptor.find_next_valid_key(["02", "29", "56", "83"], "08")
-  end
-
-  def test_calculate_possible_key_values
-    assert_equal ["01", "28", "55", "82"], @decryptor.calculate_possible_key_values(1)
-  end
-
-  def test_first_key_value
-    assert_equal "09", @decryptor.first_key_value(9)
-    assert_equal "12", @decryptor.first_key_value(12)
-  end
-
-  def test_shifts_minus_offsets_when_finding_potential_keys
-    assert_equal [8, 2, 3, 4], @decryptor.shifts_minus_offsets("vjqtbeaweqihssi", "291018")
-  end
+  # def test_find_key_with_date_and_encrypted_message
+  #   assert_equal "08304", @decryptor.find_key("vjqtbeaweqihssi", "291018")
+  #   assert_equal "14140", @decryptor.find_key("mzwet fmuen'owek'rwzifefuegmfwfeuys.
+  #    gmgwsqlrgattsmyeqdrggrx!!!twaq", "190420")
+  #   assert_equal "14811", @decryptor.find_key("mfictgskul 'ocri'yiximrdultkfcscuee.
+  #   gtkgceolytzt ekylcbrntpx!!!tcno", "200420")
+  # end
+  #
+  #
+  # def test_find_all_potential_keys
+  #   expected = {0=>["08", "35", "62", "89"], 1=>["02", "29", "56", "83"], 2=>["03", "30", "57", "84"], 3=>["04", "31", "58", "85"]}
+  #   assert_equal expected, @decryptor.find_all_potential_keys("vjqtbeaweqihssi", "291018")
+  # end
+  #
+  # def test_find_working_keys
+  #   assert_equal ["08", "83", "30", "04"], @decryptor.find_working_keys("vjqtbeaweqihssi", "291018")
+  # end
+  #
+  # def test_combine_keys
+  #   assert_equal "08304", @decryptor.combine_keys(["08", "83", "30", "04"])
+  # end
+  #
+  # def test_key_is_valid
+  #   assert_equal true, @decryptor.key_is_valid?(["02", "29", "56", "83"], "08")
+  #   assert_equal false, @decryptor.key_is_valid?(["02", "29", "56", "83"], "89")
+  # end
+  #
+  # def test_find_next_valid_key
+  #   assert_equal "83", @decryptor.find_next_valid_key(["02", "29", "56", "83"], "08")
+  # end
+  #
+  # def test_calculate_possible_key_values
+  #   assert_equal ["01", "28", "55", "82"], @decryptor.calculate_possible_key_values(1)
+  # end
+  #
+  # def test_first_key_value
+  #   assert_equal "09", @decryptor.first_key_value(9)
+  #   assert_equal "12", @decryptor.first_key_value(12)
+  # end
+  #
+  # def test_shifts_minus_offsets_when_finding_potential_keys
+  #   assert_equal [8, 2, 3, 4], @decryptor.shifts_minus_offsets("vjqtbeaweqihssi", "291018")
+  # end
 
 end
